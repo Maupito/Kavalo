a2f1e3a0d89dce8a7f8bc315aed54c5cd0aa64a6
MrPuget <bgendre.ir2024@esaip.org>
2023-09-20 14:04:09 +0200

    edit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->match(['get', 'post'],'login', 'LogController::login');
+$routes->match(['get', 'post'],'register', 'LogController::register');
+$routes->get('logout', 'LogController::logout');
--- app/Views/logs/register.php
+++ app/Views/logs/register.php
@@ -3,38 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <title>Inscription</title>
-        <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Amaranth:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <div class="navbar">
-            <a href="<?= base_url('') ?>">
-                <img src="<?= base_url('ressources/Logo_concession.png') ?>" alt="Logo">
-            </a>
-            <nav>
-            <ul>
-                <li><a href="<?= base_url('') ?>">Accueil</a></li>
-                <li>
-                    <a href="<?= base_url('voitures') ?>">Voitures</a>
-                    <ul class="submenu">
-                        <li><a href="<?= base_url('occasion') ?>">Occasion</a></li>
-                        <li><a href="<?= base_url('neuf') ?>">Neuf</a></li>
-                    </ul>
-                </li>
-                <div class="user-info">
-                    <?php if (isset($userFirstName)) { ?>
-                        Bonjour, <?= $userFirstName ?> !
-                    <?php } else { ?>
-                        <a href="<?= base_url('login') ?>">Connexion</a>
-                        <a href="<?= base_url('register') ?>">Inscription</a>
-                    <?php } ?>
-                </div>
-            </ul>
-
-        </nav>
         </div>
         <div class="content">
             <h2>Register</h2>
